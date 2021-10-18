d7a3fae70a8606bc5300e07a4107ac921302e00a
Talles Corradini <tallescorradini@gmail.com>
2021-10-18 19:54:17 -0300

    feat: get user ids from signin callback route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+});
 
 app.listen(4000, () => console.log("Server is running on port 4000"));
