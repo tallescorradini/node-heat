528e2eacbf7d319afceaebfd540bd33d7be11277
Talles Corradini <tallescorradini@gmail.com>
2021-10-19 09:42:46 -0300

    feat: implement get user profile route and functionality

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { CreateMessageController } from "./controller/CreateMessageController";
 import { GetLast3MessagesController } from "./controller/GetLast3MessagesController";
+import { GetUserProfileController } from "./controller/GetUserProfileController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -16,4 +17,9 @@ router.post(
 
 router.get("/messages/last3", new GetLast3MessagesController().handle);
 
+router.get(
+  "/profile",
+  ensureAuthenticated,
+  new GetUserProfileController().handle
+);
 export { router };
